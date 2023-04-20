402b5600b377993b08e9d03737c599f0f07be89b
Rasmus <2277443+kjellberg@users.noreply.github.com>
2023-04-20 20:30:32 +0200

    fix: update client id

--- src/utils/oauth-client.ts
+++ src/utils/oauth-client.ts
@@ -2,7 +2,7 @@ import ClientOAuth2 from 'client-oauth2'
 import { globalConfig } from '~utils/global-config.js'
 
 export const oauth = new ClientOAuth2({
-  clientId: '-FEQHRGjqiWIEmcRBXizrU3S0My797zCFVhrOLMOsPY',
+  clientId: 'UlOGBpkl9oI8yZQv43cRZNYs0qcuoZcajpIKdaNtBCA',
   accessTokenUri: `${globalConfig.get('oauthBaseUrl')}/oauth/token`,
   authorizationUri: `${globalConfig.get('oauthBaseUrl')}/oauth/authorize`,
   redirectUri: 'http://127.0.0.1:28028',
